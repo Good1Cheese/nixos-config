50ce90ba4e67ce2ac6e8d6c283d8f3308b881258
Good1Cheese <2916675@gmail.com>
2024-05-16 12:57:23 +0300

    formatted pkgs

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -6,97 +6,33 @@
 
   environment.systemPackages = with pkgs; [
     # utils
-    # any-nix-shell
-    gnumake
-    devour
-	fastfetch
-    qbittorrent
-	zoxide
-	zip
-    ntfs3g
-    ffmpeg
-    xkb-switch
-	droidcam
-    polkit_gnome
-    sqlite
-    unzip
-    python3
-    wget
-    bat
-    steam-run
-    gcc
-    cmake
-    git
-    xclip
-    stow
-    bluez
-    pcre2
-    cargo
-	nodejs_22
-    jq
+    gnumake devour fastfetch qbittorrent zip ntfs3g ffmpeg
+	xkb-switch droidcam polkit_gnome
+    sqlite unzip python3 wget bat gcc cmake
+    git xclip stow bluez pcre2 cargo nodejs_22 jq
 
     # coding
-    alacritty
-	wezterm
-    # kitty
-    pistol
-    fish
-    neovim
-    ripgrep
-    fd
-    silicon
-    bundix
-    burpsuite
+    alacritty wezterm pistol fish neovim
+    ripgrep fd ouch zoxide neovide
+    transmission unar ffmpegthumbnailer poppler silicon bundix burpsuite
 
     # workflow
-    lf
-	yazi
-    chafa
-    ueberzug
-    doas
-    broot
-    fzf
-    atool
-    btop
-    ncdu
-    du-dust
-    trash-cli
-    xdragon
-    lazygit
-    ctpv
-    ueberzugpp
-    atuin
+    lf yazi
+	chafa ueberzug doas broot fzf atool btop ncdu
+    du-dust trash-cli xdragon lazygit ctpv ueberzugpp atuin
 
     # desktop
-    xwallpaper
-    flameshot
-    redshift
-    clipmenu
+    xwallpaper flameshot redshift clipmenu
 
     # apps
-    gparted
-    tabbed
-    heroic
-    firefox
-    telegram-desktop
-    discord
-    betterdiscordctl
-    mpv
-    celluloid
-    nomacs
-    pavucontrol
-    woeusb-ng
-    r2modman
+    gparted tabbed heroic steam-run firefox telegram-desktop discord betterdiscordctl
+    mpv celluloid nomacs pavucontrol woeusb-ng r2modman
   ];
 
   fonts.packages = with pkgs; [
     # jetbrains-mono
-    noto-fonts
-    noto-fonts-emoji
-    twemoji-color-font
-    font-awesome
-    powerline-fonts
-    powerline-symbols
+    noto-fonts noto-fonts-emoji
+    twemoji-color-font font-awesome powerline-fonts powerline-symbols
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
     (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
